69c6e8b845145c45f93b8e0375f89da9ae25538d
René de Hesselle <dehesselle@web.de>
2019-03-05 23:58:14 +0100

    First iteraton of rewriting library paths

--- create_app.sh
+++ create_app.sh
@@ -12,8 +12,10 @@ WORK_DIR=/Volumes/WORK
 BREW_DIR=$WORK_DIR/homebrew
 APP_DIR=$WORK_DIR/Inkscape.app
 APP_RES_DIR=$APP_DIR/Contents/Resources
+APP_LIB_DIR=$APP_RES_DIR/lib
 INKSCAPE_BUILD=/Volumes/WORK/inkscape
 
+declare -i RECURSION=0
 
 # warning: this function has recursion!
 function get_libs
@@ -22,26 +24,56 @@ function get_libs
   local remember=$2
 
   local line
-  local lib
 
   if [ $(grep "$file" $remember | wc -l) -eq 0 ]; then
     echo $file >> $remember   # remember files we've already processed
                               # to avoid endless recursion
 
+
+    if [ $RECURSION -eq 0 ]; then   # first run is inkscape binary
+      :
+      cp $file $APP_DIR/Contents/MacOS
+    else
+      :
+      cp $file $APP_LIB_DIR
+      chmod 644 $APP_LIB_DIR/*    # TODO on file basis, plz!
+      #install_name_tool -change $lib @executable_path/../Resources/lib/$(basename $lib) $APP_LIB_DIR/$(basename $file)
+    fi
+
     otool -L $file | while IFS="" read -r line; do
       if [[ $line =~ [^/]+(/Volumes/WORK/homebrew/[^\  ]+).* ]]; then
-        lib=${BASH_REMATCH[1]}   # extract fully qualified filename
+        local lib=${BASH_REMATCH[1]}   # extract fully qualified filename
 
         if [ "$file" != "$lib" ]; then   # skip "self" in otool output
           echo "lib>$lib<"
+          ((RECURSION++))
           get_libs $lib $remember
+          ((RECURSION--))
+          
+          if [ $RECURSION -eq 0 ]; then   # first run is inkscape binary
+            :
+          else
+            :
+            install_name_tool -change $lib @executable_path/../Resources/lib/$(basename $lib) $APP_LIB_DIR/$(basename $file)
+          fi
+          
+        
           # TODO rewrite path to rpath >>>here<<<<
           #break    # for debugging: this breaks recursion
+        else
+          echo "i got here"
+          echo "install_name_tool -id Inkscape.app/Contents/Resources/lib/$(basename $lib) $APP_LIB_DIR/$(basename $file)"
+          install_name_tool -id Inkscape.app/Contents/Resources/lib/$(basename $lib) $APP_LIB_DIR/$(basename $file)
+          #exit 0
+        
         fi
       fi
     done
   fi
 }
+
+mkdir -p $APP_DIR/Contents/MacOS
+mkdir -p $APP_LIB_DIR
 
 > $WORK_DIR/remember.txt
 get_libs $INKSCAPE_BUILD/bin/inkscape $WORK_DIR/remember.txt
@@ -49,11 +81,9 @@ get_libs $INKSCAPE_BUILD/bin/inkscape $WORK_DIR/remember.txt
 
 exit 0 ####################### exit for now
 
-mkdir -p $APP_DIR/Contents/MacOS
-mkdir -p $APP_RES_DIR
-
 
 cp -R $INKSCAPE_BUILD/lib $APP_RES_DIR
 cp -R $INKSCPAE_BUILD/share $APP_RES_DIR
+
 
 
