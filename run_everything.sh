5c7246a7f3fd283595f4c51beceef9f3ded5d3f3
René de Hesselle <dehesselle@web.de>
2019-04-01 01:01:36 +0200

    Rename files to group activities together

--- run_everything.sh
+++ run_everything.sh
@@ -2,15 +2,9 @@
 # run-everything.sh
 # https://github.com/dehesselle/mibap
 #
-# do everything "from source to app"
+# create jhbuild environment for Inkscape
 
-SELF_DIR=$(cd $(dirname "$0"); pwd -P)
-TIMESTAMP=$(date +%y%m%d%H%M%S)
-LOG=$SELF_DIR/run_everything-$TIMESTAMP.log
-
-{ time ./030-jhbuild-bootstrap.sh 2> 030-$TIMESTAMP.log ; } 2>>$LOG
-{ time ./040-jhbuild-python2.sh 2> 040-$TIMESTAMP.log ; } 2>>$LOG
-{ time ./050-jhbuild-gtk3.sh 2> 050-$TIMESTAMP.log ; } 2>>$LOG
-{ time ./060-jhbuild-inkdeps.sh 2> 060-$TIMESTAMP.log ; } 2>>$LOG
-{ time ./070-inkscape.sh 2> 070-$TIMESTAMP.log ; } 2>>$LOG
+for script in 1??-*.sh; do
+  echo "./$script"
+done
 
