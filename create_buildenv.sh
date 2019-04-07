cb5f6c14101f1b8f1abedbb98776d3fc0887e410
René de Hesselle <dehesselle@web.de>
2019-04-07 18:46:59 +0200

    Fix file header for Inkscape

--- create_buildenv.sh
+++ create_buildenv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
-# create_buildenv.sh
-# https://github.com/dehesselle/mibap
+# SPDX-License-Identifier: GPL-2.0-or-later
 #
+# This file is part of the build pipeline for Inkscape on macOS.
+#
+# ### create_buildenv.sh ###
 # create jhbuild environment for Inkscape
 
 for script in 1??-*.sh; do
