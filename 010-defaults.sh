3c4f2f8a1df6701569a68339d1961b5266962ac8
René de Hesselle <dehesselle@web.de>
2019-04-07 15:02:12 +0200

    Add new defaults file
    Renumber the existing files to make room.

---
+++ 010-defaults.sh
@@ -0,0 +1,10 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+#
+# This file is part of the build pipeline for Inkscape on macOS.
+#
+# ### 010-defaults.sh ###
+# This file contains a few default values that YOU ARE NOT SUPPOSED TO CHANGE.
+# The reason is that there are certain checks and dependencies that are of out
+# your individual control and the other scripts blindly trust that the defaults
+# set in this file are "the truth". You have been warned.
+
