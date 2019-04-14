139308ae53e418d5f6591295487ecfcb421337c5
René de Hesselle <dehesselle@web.de>
2019-04-14 16:51:42 +0200

    Remove jhbuild configuration files
    There proofs to be no reason to patch them like before, so this is
    a simplification yielding the same effect.

--- 110-jhbuild-bootstrap.sh
+++ 110-jhbuild-bootstrap.sh
@@ -35,31 +35,28 @@ mkdir -p $SRC_DIR/download     # downloaded tarballs
 # WARNING: Operations like this are the reason that you're supposed to use
 # a dedicated machine for building. This script does not care for your
 # data.
-rm -rf $HOME/.cache $HOME/.local $HOME/Source   # remove remnants of previous run
+rm -rf $HOME/.cache $HOME/.local $HOME/Source
 ln -sf $TMP_DIR $HOME/.cache   # link to our workspace
 ln -sf $OPT_DIR $HOME/.local   # link to our workspace
 ln -sf $SRC_DIR $HOME/Source   # link to our workspace
 
 ### install and configure jhbuild ##############################################
 
+# remove configuration files from a previous installation
+rm $HOME/.jhbuild*
+
+# install jhbuild
 cd $WRK_DIR
 bash <(curl -s $URL_GTK_OSX_BUILD_SETUP)   # run jhbuild setup script
 
-JHBUILDRC=$HOME/.jhbuildrc-custom
-if [ -f $JHBUILDRC ]; then   # remove previous configuration from end of file
-  LINE_NO=$(grep -n "# And more..." $JHBUILDRC | awk -F ":" '{ print $1 }')
-  head -n +$LINE_NO $JHBUILDRC >$JHBUILDRC.clean
-  mv $JHBUILDRC.clean $JHBUILDRC
-  unset LINE_NO
-fi
-
 # configure jhbuild
-echo "checkoutroot = '$SRC_DIR/checkout'" >> $JHBUILDRC
-echo "prefix = '$OPT_DIR'" >> $JHBUILDRC
-echo "tarballdir = '$SRC_DIR/download'" >> $JHBUILDRC
-echo "quiet_mode = True" >> $JHBUILDRC    # suppress all build output
-echo "progress_bar = True" >> $JHBUILDRC
-echo "moduleset = '$URL_GTK_OSX_MODULESET'" >> $JHBUILDRC
+JHBUILDRC=$HOME/.jhbuildrc-custom
+echo "checkoutroot = '$SRC_DIR/checkout'"   >> $JHBUILDRC
+echo "prefix = '$OPT_DIR'"                  >> $JHBUILDRC
+echo "tarballdir = '$SRC_DIR/download'"     >> $JHBUILDRC
+echo "quiet_mode = True"                    >> $JHBUILDRC    # suppress output
+echo "progress_bar = True"                  >> $JHBUILDRC
+echo "moduleset = '$URL_GTK_OSX_MODULESET'" >> $JHBUILDRC    # custom moduleset
 
 ### bootstrap jhbuild environment ##############################################
 
