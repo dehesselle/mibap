8748b6079f118c373cee4d2759d5699a72d8c3fc
René de Hesselle <dehesselle@web.de>
2019-04-19 23:35:31 +0200

    Check if build environment exists

--- create_buildenv.sh
+++ create_buildenv.sh
@@ -13,12 +13,16 @@ for script in $SELF_DIR/0??-*.sh; do source $script; done
 
 ### download pre-built build environment or build from scratch #################
 
-if $PREBUILT_BUILDENV_ENABLE &&
-   [ "$WRK_DIR" = "$DEFAULT_SYSTEM_WRK_DIR" ]; then   # we're good to download
-  $SELF_DIR/110-jhbuild-install.sh
-  get_source $URL_PREBUILT_BUILDENV $WRK_DIR
-else  # we need to build from scratch
-  for script in $SELF_DIR/1??-*.sh; do
-    $script
-  done
+if [ -d $OPT_DIR ]; then
+  echo "create_buildenv.sh: $OPT_DIR exists - not doing anything"
+else
+  if $PREBUILT_BUILDENV_ENABLE &&
+     [ "$WRK_DIR" = "$DEFAULT_SYSTEM_WRK_DIR" ]; then   # we're good to download
+    $SELF_DIR/110-jhbuild-install.sh
+    get_source $URL_PREBUILT_BUILDENV $WRK_DIR
+  else  # we need to build from scratch
+    for script in $SELF_DIR/1??-*.sh; do
+      $script
+    done
+  fi
 fi
