a96dc8a064bea952149bb9df1a2040f271312353
René de Hesselle <dehesselle@web.de>
2019-06-09 16:31:04 +0200

    Set deployment target to macOS Sierra

--- 020-vars.sh
+++ 020-vars.sh
@@ -27,6 +27,12 @@ SELF_NAME=$(basename $0)
 CORES=$(sysctl -n hw.ncpu)   # use all available cores
 export MAKEFLAGS="-j $CORES"
 
+### target OS version ##########################################################
+
+# Using 10.13 SDK it's not safe to go lower than 10.12.
+# https://smallhacks.wordpress.com/2018/11/11/how-to-support-old-osx-version-with-a-recent-xcode
+export MACOSX_DEPLOYMENT_TARGET=10.12   # minimum version macOS Sierra
+
 ### ramdisk ####################################################################
 
 RAMDISK_ENABLE=true   # mount ramdisk to $WRK_DIR
