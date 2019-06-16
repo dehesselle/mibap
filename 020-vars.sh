aa810ad62d8da53f2a51f44f6f7af1738f75e769
René de Hesselle <dehesselle@web.de>
2019-06-16 11:05:35 +0200

    Update Python for 10.9 target

--- 020-vars.sh
+++ 020-vars.sh
@@ -170,7 +170,7 @@ URL_OPENMP=https://github.com/llvm/llvm-project/releases/download/llvmorg-7.1.0/
 URL_OPENSSL=https://www.openssl.org/source/openssl-1.1.0g.tar.gz
 URL_POPPLER=https://gitlab.freedesktop.org/poppler/poppler/-/archive/poppler-0.74.0/poppler-poppler-0.74.0.tar.gz
 URL_POTRACE=http://potrace.sourceforge.net/download/1.15/potrace-1.15.tar.gz
-URL_PYTHON3=https://github.com/dehesselle/py3framework/releases/download/py368.1/py368_framework_1.tar.xz
+URL_PYTHON3=https://github.com/dehesselle/py3framework/releases/download/py368.3/py368_framework_3.tar.xz
 # A pre-built version of the complete toolset.
 URL_TOOLSET_CACHE=https://github.com/dehesselle/mibap/releases/download/v0.11/mibap_v0.11.tar.xz
 
