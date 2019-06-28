94d14f08af59a3a1aad7329e47fdaee5a94e59ef
René de Hesselle <dehesselle@web.de>
2019-06-28 22:15:18 +0200

    Create '.dmg'

--- 230-inkscape-distrib.sh
+++ 230-inkscape-distrib.sh
@@ -15,9 +15,33 @@ set -e
 
 ### create disk image for distribution #########################################
 
-# TODO: *.dmg, signing, notarization, ...
+cp $SELF_DIR/inkscape_dmg.json $SRC_DIR
+
+(
+  # escaping forward slashes: https://unix.stackexchange.com/a/379577
+  # ESCAPED=${STRING//\//\\\/}
+
+  FILE=$SRC_DIR/inkscape_dmg.png
+  sed -i '' "s/PLACEHOLDERBACKGROUND/${FILE//\//\\\/}/" $SRC_DIR/inkscape_dmg.json
+  sed -i '' "s/PLACEHOLDERPATH/${APP_DIR//\//\\\/}/" $SRC_DIR/inkscape_dmg.json
+)
+
+# create background
+convert -size 560x400 xc:transparent \
+  -font Andale-Mono -pointsize 64 -fill black \
+  -draw "text 20,60 'Inkscape'" \
+  -draw "text 20,120 'development'" \
+  -draw "text 20,180 'snapshot'" \
+  -draw "text 20,240 '$(get_repo_version $INK_DIR)'" \
+  $SRC_DIR/inkscape_dmg.png
+
+(
+  cd $SRC_DIR/node-*
+  export PATH=$PATH:$(pwd)/bin
+  appdmg $SRC_DIR/inkscape_dmg.json $ARTIFACT_DIR/Inkscape.dmg
+)
 
 if [ ! -z $CI_JOB_ID ]; then   # create build artifcat for CI job
-  cd $WRK_DIR
   mv $ARTIFACT_DIR $INK_DIR/artifacts
+  rm -rf $INK_DIR/artifacts/*.app
 fi
