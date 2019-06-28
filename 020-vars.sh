b6839d191ed487d1a8de78768c3a78370e7812be
René de Hesselle <dehesselle@web.de>
2019-06-28 21:12:06 +0200

    Add Node.js and appdmg
    appdmg is the tool to package Inkscape as '.dmg'

--- 020-vars.sh
+++ 020-vars.sh
@@ -171,6 +171,7 @@ URL_LIBREVENGE=https://ayera.dl.sourceforge.net/project/libwpd/librevenge/librev
 URL_LIBSOUP=https://ftp.gnome.org/pub/GNOME/sources/libsoup/2.65/libsoup-2.65.92.tar.xz
 URL_LIBVISIO=https://github.com/LibreOffice/libvisio/archive/libvisio-0.1.6.tar.gz
 URL_LIBWPG=https://netcologne.dl.sourceforge.net/project/libwpg/libwpg/libwpg-0.3.3/libwpg-0.3.3.tar.xz
+URL_NODEJS=https://nodejs.org/dist/v10.16.0/node-v10.16.0-darwin-x64.tar.gz
 URL_OPENJPEG=https://github.com/uclouvain/openjpeg/archive/v2.3.0.tar.gz
 URL_OPENMP=https://github.com/llvm/llvm-project/releases/download/llvmorg-7.1.0/openmp-7.1.0.src.tar.xz
 # use OpenSSL version as in gtk-osx moduleset
