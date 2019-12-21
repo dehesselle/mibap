dcd3410406df005cc3656b91d8fd825abaa170d2
René de Hesselle <dehesselle@web.de>
2019-12-21 22:43:22 +0100

    Fix superfluous 'if' and indentation

--- install_toolset.sh
+++ install_toolset.sh
@@ -19,46 +19,45 @@ set -e
 
 function install
 {
-    local toolset_dmg=$TOOLSET_REPO_DIR/$(basename $URL_TOOLSET)
+  local toolset_dmg=$TOOLSET_REPO_DIR/$(basename $URL_TOOLSET)
 
-    if [ -f $toolset_dmg ]; then
-      echo_info "no download required"
-    else
-      # File not present on disk, we need to download.
-      echo_act "download required"
-      save_file $URL_TOOLSET $TOOLSET_REPO_DIR
-      echo_ok "download successful"
-    fi
-
-    # mount build system read-only
-    local device=$(create_dmg_device $toolset_dmg)
-    [ ! -d $WRK_DIR ] && mkdir -p $WRK_DIR
-    mount -o nobrowse,ro -t hfs $device $WRK_DIR
-    echo_ok "toolset mounted as $device"
-
-    # Sadly, there are some limitations involved with union-mounting:
-    #   - Files are not visible to 'ls'.
-    #   - You cannot write in a location without having written to its
-    #     parent location. That's why we need to pre-create all directories
-    #     below.
-    #
-    # Shadow-mounting a dmg is not a feasible alternative due to its
-    # bad write-performance.
-
-    # prepare a script for mass-creating directories
-    find $OPT_DIR -type d ! -path "$TMP_DIR/*" ! -path "$SRC_DIR/*" \
-        -exec echo "mkdir {}" > $TOOLSET_ROOT_DIR/create_dirs.sh \;
-    chmod 755 $TOOLSET_ROOT_DIR/create_dirs.sh
-
-    # create writable (ramdisk-) overlay
-    device=$(create_ram_device $OVERLAY_RAMDISK_SIZE build)
-    mount -o nobrowse,rw,union -t hfs $device $WRK_DIR
-    echo_ok "writable ramdisk overlay mounted as $device"
-
-    # create all directories inside overlay
-    $TOOLSET_ROOT_DIR/create_dirs.sh
-    rm $TOOLSET_ROOT_DIR/create_dirs.sh
+  if [ -f $toolset_dmg ]; then
+    echo_info "no download required"
+  else
+    # File not present on disk, we need to download.
+    echo_act "download required"
+    save_file $URL_TOOLSET $TOOLSET_REPO_DIR
+    echo_ok "download successful"
   fi
+
+  # mount build system read-only
+  local device=$(create_dmg_device $toolset_dmg)
+  [ ! -d $WRK_DIR ] && mkdir -p $WRK_DIR
+  mount -o nobrowse,ro -t hfs $device $WRK_DIR
+  echo_ok "toolset mounted as $device"
+
+  # Sadly, there are some limitations involved with union-mounting:
+  #   - Files are not visible to 'ls'.
+  #   - You cannot write in a location without having written to its
+  #     parent location. That's why we need to pre-create all directories
+  #     below.
+  #
+  # Shadow-mounting a dmg is not a feasible alternative due to its
+  # bad write-performance.
+
+  # prepare a script for mass-creating directories
+  find $OPT_DIR -type d ! -path "$TMP_DIR/*" ! -path "$SRC_DIR/*" \
+      -exec echo "mkdir {}" > $TOOLSET_ROOT_DIR/create_dirs.sh \;
+  chmod 755 $TOOLSET_ROOT_DIR/create_dirs.sh
+
+  # create writable (ramdisk-) overlay
+  device=$(create_ram_device $OVERLAY_RAMDISK_SIZE build)
+  mount -o nobrowse,rw,union -t hfs $device $WRK_DIR
+  echo_ok "writable ramdisk overlay mounted as $device"
+
+  # create all directories inside overlay
+  $TOOLSET_ROOT_DIR/create_dirs.sh
+  rm $TOOLSET_ROOT_DIR/create_dirs.sh
 }
 
 ### main #######################################################################
