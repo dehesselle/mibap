01bd0680843a1f52236d81125d9b34fec0d7466e
René de Hesselle <dehesselle@web.de>
2020-08-17 00:02:56 +0200

    Switch to inkscape-1.1.x module set

--- 020-vars.sh
+++ 020-vars.sh
@@ -33,7 +33,7 @@ export SDKROOT=$SDKROOT_DIR/MacOSX${MACOSX_DEPLOYMENT_TARGET}.sdk
 
 ### build system/toolset version ###############################################
 
-TOOLSET_VERSION=0.37
+TOOLSET_VERSION=0.38
 
 ### ramdisk ####################################################################
 
@@ -139,9 +139,9 @@ PY3_BUILD=1  # custom framework build number
 
 # https://github.com/dehesselle/gtk-osx
 # Forked from https://gitlab.gnome.org/GNOME/gtk-osx
-URL_GTK_OSX=https://raw.githubusercontent.com/dehesselle/gtk-osx/inkscape-1.0.x-7
+URL_GTK_OSX=https://raw.githubusercontent.com/dehesselle/gtk-osx/inkscape-1.1.x   # FIXME: unversioned
 URL_GTK_OSX_SETUP=$URL_GTK_OSX/gtk-osx-setup.sh
-URL_GTK_OSX_MODULESET=$URL_GTK_OSX/modulesets-stable/gtk-osx.modules
+URL_GTK_OSX_MODULESET=$URL_GTK_OSX/modulesets-stable/inkscape.modules
 
 ### download URLs for auxiliary software #######################################
 
