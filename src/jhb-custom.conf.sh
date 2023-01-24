64903650f4b6ede4c57f0eb90cc9872911238027
René de Hesselle <dehesselle@web.de>
2023-01-24 21:48:20 +0100

    Apply updates from gtk-osx
    THe massive amount of changes is due to a changed formatting by
    using HTML tidy.
    
    Add a patch for GTK 3.24.36 to fix a window sizing problem.

--- src/jhb-custom.conf.sh
+++ src/jhb-custom.conf.sh
@@ -18,7 +18,7 @@
 
 ### variables ##################################################################
 
-VERSION=0.70
+VERSION=0.71
 VER_DIR_TEMPLATE="\$WRK_DIR/mibap-\$VERSION"
 
 RELEASE_ARCHIVE=mibap-"$VERSION"_$(uname -m).dmg
