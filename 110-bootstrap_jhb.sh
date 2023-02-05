c7690c8d84a22b32e291e3df6b34fb3759550113
René de Hesselle <dehesselle@web.de>
2023-02-05 19:07:10 +0100

    Add PACKAGE_CACHE_DIR
    This is a way to provide packages offline, avoiding downloads.

--- 110-bootstrap_jhb.sh
+++ 110-bootstrap_jhb.sh
@@ -22,10 +22,53 @@
 
 SELF_DIR=$(dirname "${BASH_SOURCE[0]}")
 
+PACKAGE_CACHE_DIR=${PACKAGE_CACHE_DIR:-""}
+
 ### functions ##################################################################
 
-# Nothing here.
+function preload
+{
+  local source_dir=$1
+
+  source "$SELF_DIR/jhb/etc/jhb.conf.sh" "$SELF_DIR/src/jhb-custom.conf.sh"
+  mkdir -p "$PKG_DIR"
+
+  echo "using PACKAGE_CACHE_DIR=$PACKAGE_CACHE_DIR"
+
+  local xp_elements="*[self::autotools or self::cmake or self::distutils or \
+self::meson]"
+
+  # Iterate over every module in every moduleset and check if the file is
+  # available in source_dir. If it is, copy it to PKG_DIR.
+  for modules in "$SELF_DIR"/jhb/etc/modulesets/jhb/*.modules \
+                 "$SELF_DIR"/modulesets/*.modules; do
+    local count
+    count=$(xmllint --xpath "count(//moduleset/$xp_elements)" "$modules")
+    local i=1
+    while [ $i -le "$count" ]; do
+      local package
+      # shellcheck disable=SC1087 # this is no array expansion
+      package=$(xmllint --xpath "string(//moduleset/\
+$xp_elements[$i]/branch/@rename-tarball)" "$modules")
+      if [ -z "$package" ]; then
+        # shellcheck disable=SC1087 # this is no array expansion
+        package=$(xmllint --xpath "string(//moduleset/\
+$xp_elements[$i]/branch/@module)" "$modules")
+      fi
+
+      package=$source_dir/$(basename "$package")
+      if [ -f "$package" ]; then
+        cp "$package" "$PKG_DIR"
+      fi
+      ((i++))
+    done
+  done
+}
 
 ### main #######################################################################
+
+if [ -d "$PACKAGE_CACHE_DIR" ]; then
+  preload "$PACKAGE_CACHE_DIR"
+fi
 
 "$SELF_DIR"/jhb/usr/bin/bootstrap "$SELF_DIR"/src/jhb-custom.conf.sh
